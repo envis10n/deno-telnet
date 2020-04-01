1c159e19ef32a4036ba97aedf4489acdd45b6b35
envis10n <envis10n@protonmail.com>
2020-04-01 01:30:17 -0500

    Add telnet server to examples.

---
+++ examples/telnet_server.ts
@@ -0,0 +1,50 @@
+import * as Telnet from "../mod.ts";
+
+const server = Deno.listen(
+  {
+    port: Deno.args[0] !== undefined ? Number(Deno.args[0]) : 13337,
+    hostname: Deno.args[1] !== undefined ? Deno.args[1] : "127.0.0.1",
+    transport: "tcp",
+  },
+);
+
+for await (const conn of server) {
+  console.log("Client connected from:", conn.remoteAddr);
+  async function send(data: string): Promise<number> {
+    const bytes = new TextEncoder().encode(data + "\r\n");
+    await conn.write(bytes);
+    return await conn.write(Telnet.buildTelnetCommand(Telnet.Command.GA));
+  }
+  const parser: Telnet.Parser = new Telnet.Parser();
+  parser.on("data", (chunk) => {
+    const t = new TextDecoder().decode(chunk);
+    console.log("Text:", t);
+    send(`echo> ${t}`);
+  });
+  parser.on("negotiation", (command, option) => {
+    console.log("Negotiation:", Telnet.Command[command], Telnet.Option[option]);
+  });
+  parser.on("subnegotiation", (option, data) => {
+    console.log("Subnegotiation:", Telnet.Option[option]);
+  });
+  parser.on("gmcp", (namespace, data) => {
+    console.log("GMCP:", namespace, "|", data);
+  });
+  conn.write(
+    Telnet.buildTelnetCommand(Telnet.Command.WILL, Telnet.Option.GMCP),
+  );
+  send("Welcome to the Deno Telnet server!");
+  (async () => {
+    while (true) {
+      let buf = new Uint8Array(1024);
+      const res = await conn.read(buf);
+      if (res === Deno.EOF) {
+        // EOF
+        break;
+      } else {
+        parser.accumulate(buf.slice(0, res));
+      }
+    }
+    console.log("Client disconnected.");
+  })();
+}
